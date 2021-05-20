71ed9a02d4f603c4f78e8c6c328b5eea732c8000
Nazar Matsevych <nazar6192111@gmail.com>
2021-05-20 14:59:50 +0300

    Updates

---
+++ precache-manifest.498cd69dbb8a389ee8e7d4169501f2d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "096ff806a87c8189c5695efe8c49fd38",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ab24659a2d5530a88852",
+    "url": "./static/css/main.02088be7.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab24659a2d5530a88852",
+    "url": "./static/js/main.0ccf2d81.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
